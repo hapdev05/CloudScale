9b110bff0bcf2aec2e60293dd864c39e489c8aa8
hapdev05 <demoover81@gmail.com>
2026-09-16 09:08:29 +0700

    fix: Use gzip .tar.gz for Node.js download

--- terraform/launch-template.tf
+++ terraform/launch-template.tf
@@ -30,8 +30,8 @@ resource "aws_launch_template" "app" {
               exec > /var/log/user-data.log 2>&1
               set -x
 
-              # Fast download pre-compiled Node.js 18 binary tarball (under 3s)
-              curl -fsSL https://nodejs.org/dist/v18.20.2/node-v18.20.2-linux-x86_64.tar.xz | tar -xJ --strip-components=1 -C /usr/local
+              # Fast download pre-compiled Node.js 18 binary tarball (.tar.gz - zero dependencies)
+              curl -fsSL https://nodejs.org/dist/v18.20.2/node-v18.20.2-linux-x86_64.tar.gz | tar -xz --strip-components=1 -C /usr/local
 
               mkdir -p /home/ubuntu/app
               cd /home/ubuntu/app
